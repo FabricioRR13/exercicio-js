f484eb6870726d6572a364bca7b530b9862de95b
FabricioRR13 <fabricio.r@aluno.senai.br>
2025-03-20 17:59:01 -0300

    exercico js

---
+++ ex.js
@@ -0,0 +1,108 @@
+//  let numero = prompt("Digite um número");
+
+// numero = parseInt(numero)
+
+//  if(numero > 0){
+//      console.log("Número positivo");
+//  }else if(numero == 0){
+//      console.log("Número igual a zero");
+//  }else if(numero < 0){
+//      console.log("Numero negativo");
+// }
+
+
+//exercicio 2
+
+// let numero = prompt("Digite um número");
+
+// if(numero % 2 === 0){
+//     console.log("Numero par")
+// } else if (numero % 2 !== 0) {
+// console.log("numero impar");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+//exercicio 3
+
+// let idade = prompt("Digite sua idade");
+// if (idade >= 0 && idade < 12){
+//     console.log("Criança");
+// }if (idade >= 13 && idade < 17){
+//     console.log("Adolecente");
+// }if (idade >= 18){
+//     console.log("Adulto")
+// }
+
+//exercicio 4
+
+// let nota = prompt("Digite sua nota");
+
+// if(nota >=60){
+//     console.log("Aprovado");
+// }else{
+//     console.log("Reprovado");
+// }
+
+// exercicio Intermediario 1
+
+ let numero1 =  prompt("Digite o primeiro numero")
+ let numero2 = prompt("Digite o segundo numero")
+ let operacao = prompt("Digite a operacao")
+
+ switch (operacao){
+ case 1:
+    let soma = numero1 + numero2;
+    console.log("soma");
+    break;
+    case 2:
+    let subtracao = numero1 - numero2;
+    console.log("subtracao")
+    break;
+    case 3:
+    let multiplicacao = numero1 * numero3;
+    console.log("multiplicacao")
+    break;
+    case 4:
+        let divisao = numero % numero2;
+        break;
+        
+ }
+
+
+ exercicio inter 2
+
+ let numero1 =  prompt("Digite o primeiro numero")
+ let numero2 = prompt("Digite o segundo numero")
+ let operacao = prompt("Digite o terceiro numero")
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ script.js
@@ -0,0 +1,112 @@
+// let nome = prompt("Digite seu nome: ");
+// //console.log("Seu nome é:" +nome);
+// alert("Seu nome é: " + nome);
+
+//if SIMPLES
+// let idade = prompt("Digite sua idade:");
+
+// if(idade >=18){
+//     console.log("Você é maior de idade");
+// }
+
+
+
+//IF/ ELSE
+
+
+
+// let TEMPERATURA = 30;
+
+// if(TEMPERATURA >25) {
+//     console.log("ESTA CALOR");
+
+
+// }else{
+//     console.log("Está frio");
+// }
+
+//if/else if/ else
+
+// let nota = 5;
+
+// if(nota > 6) {
+
+//     console.log("Aprovado(a)!");
+
+// }else if(nota >= 5){
+//     console.log("Está de recuperação");
+// }else{
+//         console.log("reprovado");
+//     }
+
+
+
+//OPERADOR TÉRNARIO / IF TERNARIO
+
+// let saldo = 110;
+
+// let podeComprar = saldo <= 100 ? "Compra aprovada!" : "Saldo insuficiente.";
+// console.log(podeComprar + "obrigado");
+
+//SWITCH CASE
+
+// let dia = 5;
+
+// switch (dia){
+//     case 1:
+//         console.log("Segunda-feira");
+//         break;
+//         case 2: 
+//         console.log("Terça feira");
+//         break;
+//         case 3:
+//             console.log("Quarta-feira");
+//             break;
+//             default:
+//                 console.log("Dia inválido.");
+
+// }
+
+
+//CONDICIONAL ANINHADA
+
+// let idade = 16;
+// let temCarteira = false;
+
+
+// if(idade >=18){
+//     if(temCarteira){
+//         console.log ("Você pode dirigir.");
+//     }else{
+//         console.log("Você precisa tirar sua cartyeira de motorista");
+
+//     }
+// }else{
+//     console.log("Você ainda não pode dirigir.");
+// }
+
+
+
+//CONDIÇÃO ANINHADA
+//MULTIPLAS VERIFICAÇÕES
+
+// let hora = 20;
+// let diaDaSemana = "segunda";
+
+// if(hora >= 6 && hora < 12){
+//     console.log("Bom dia!");
+// }else{
+//     if(hora >= 12 && hora < 18) {
+//         if (diaDaSemana === "segunda") {
+//             console.log("Boa tarde Ótima semana")
+//         } else{
+//             console.log("Boa tarde!");
+        
+//         }
+//     } else {
+//         if(hora >= 18 && hora < 24){
+//         console.log("Boa noite");
+
+//     }
+// }
+// }
